becd1d0dcf61d5a58ce021d0446cc0163bda1e86
martinamineo98 <martinamineo98@gmail.com>
2022-08-27 09:56:41 +0200

    Add dropdownmenu

---
+++ dropdownmenu/app.js
@@ -0,0 +1,18 @@
+
+const dropdownMenu = (function () {
+  const menuParents = document.querySelectorAll('.menu-parent')
+  const menuChildren = document.querySelectorAll('.menu-child')
+  
+  function turnInvisible () {
+		menuChildren.forEach((child) => child.classList.remove('isVisible'))
+	}
+  
+  menuParents.forEach((parent) => {
+		parent.addEventListener('click', () => {
+			const child = parent.querySelector('ul.menu-child')
+			turnInvisible()
+			child.offsetWidth
+			child.classList.add('isVisible')
+		})
+	})
+})()
