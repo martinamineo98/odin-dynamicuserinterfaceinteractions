601746dce18f3f85462c61a343f86c6334b75ec1
martinamineo98 <martinamineo98@gmail.com>
2022-08-27 12:13:40 +0200

    Fixed mobilemenu/app.js

--- mobilemenu/app.js
+++ mobilemenu/app.js
@@ -1,10 +1,10 @@
 
 const addButtonAnimation = (function () {
-	const menuUl = document.querySelector('.menu ul')
-	const button = document.querySelector('.menu button')
-				button.addEventListener('click', () => {
-					button.classList.toggle('isAnimation')
-					menuUl.classList.toggle('isVisible')
-					setTimeout(() => button.classList.toggle('isAnimation'), 750)
-				})
+  const menuUl = document.querySelector('.menu ul')
+  const button = document.querySelector('.menu button')
+  button.addEventListener('click', () => {
+    button.classList.toggle('isAnimation')
+    menuUl.classList.toggle('isVisible')
+    setTimeout(() => button.classList.toggle('isAnimation'), 750)
+  })
 })()
