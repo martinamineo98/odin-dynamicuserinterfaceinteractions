c79bc9e048f3ea7b2b0527e85cb44996a8a0150e
martinamineo98 <martinamineo98@gmail.com>
2022-08-27 09:58:27 +0200

    Fixed dropdownmenu/app.js

--- dropdownmenu/app.js
+++ dropdownmenu/app.js
@@ -2,17 +2,16 @@
 const dropdownMenu = (function () {
   const menuParents = document.querySelectorAll('.menu-parent')
   const menuChildren = document.querySelectorAll('.menu-child')
-  
+
   function turnInvisible () {
-		menuChildren.forEach((child) => child.classList.remove('isVisible'))
-	}
-  
+    menuChildren.forEach((child) => child.classList.remove('isVisible'))
+  }
+
   menuParents.forEach((parent) => {
-		parent.addEventListener('click', () => {
-			const child = parent.querySelector('ul.menu-child')
-			turnInvisible()
-			child.offsetWidth
-			child.classList.add('isVisible')
-		})
-	})
+    parent.addEventListener('click', () => {
+      const child = parent.querySelector('ul.menu-child')
+      turnInvisible()
+      child.classList.add('isVisible')
+    })
+  })
 })()
